209bba4aae3c3720e447592dd7ae69619e5f87d7
unknown <razume2@gmail.com>
2025-04-29 16:20:33 +0600

    Post blog show and create

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return inertia('Home');
+// Route::inertia('/','Home');
+
+Route::get('/',[PostController::class,'index']);
+Route::resource('posts',PostController::class)->except('index');
+
+Route::get('/about', function () {
+    return Inertia::render('About/About');
 });
+
